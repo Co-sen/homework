dce58d3e64927041af9faabd164d3aa45166f2b1
Co-sen <71631167+Co-sen@users.noreply.github.com>
2020-09-21 21:57:56 +0800

    Add files via upload

---
+++ Picture(1).java
@@ -0,0 +1,76 @@
+
+/**
+ * This class represents a simple picture. You can draw the picture using
+ * the draw method. But wait, there's more: being an electronic picture, it
+ * can be changed. You can set it to black-and-white display and back to
+ * colors (only after it's been drawn, of course).
+ *
+ * This class was written as an early example for teaching Java with BlueJ.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 1.1  (24 May 2001)
+ */
+public class Picture
+{
+    private Square wall;
+    private Square window;
+    private Triangle roof;
+    private Circle sun;
+
+    /**
+     * Constructor for objects of class Picture
+     */
+    public Picture()
+    {
+        // nothing to do... instance variables are automatically set to null
+    }
+
+    /**
+     * Draw this picture.
+     */
+    public void draw()
+    {
+        sun = new Circle();
+        sun.changeColor("black");
+        sun.moveHorizontal(10);
+        sun.moveVertical(-10);
+        sun.changeSize(60);
+        sun.makeVisible();
+        sun.moveHorizontal(70);
+    }
+
+    /**
+     * Change this picture to black/white display
+     */
+    public void setBlackAndWhite()
+    {
+        if(wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("black");
+            window.changeColor("white");
+            roof.changeColor("black");
+            sun.changeColor("black");
+        }
+    }
+
+    /**
+     * Change this picture to use color display
+     */
+    public void setColor()
+    {
+        if(wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("red");
+            window.changeColor("black");
+            roof.changeColor("green");
+            sun.changeColor("yellow");
+        }
+    }
+
+    public static void main(String[] args) {
+        Picture picture = new Picture();
+//        picture.setBlackAndWhite();
+        picture.draw();;
+    }
+
+}
